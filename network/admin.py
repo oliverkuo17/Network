79b00f9be881029c4ea8bd1c573207688511a491
oliverkuo17 <oliverkuo17@gmail.com>
2021-03-10 15:26:29 +1100

    implement follow function

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Post, Follower
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Follower)
--- network/views.py
+++ network/views.py
@@ -1,4 +1,5 @@
 import json
+from django.contrib.admin.utils import flatten
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -96,5 +97,23 @@ def post(request, post_id):
         post.save()
         return JsonResponse({"message": "Post edited successfully", "likes_num": post.likes.count()}, status=201)
 
-def profile(request):
-    pass
+
+def profile(request, user_id):
+    user_profile = User.objects.get(pk=user_id)
+    all_posts = Post.objects.filter(user=user_id).order_by("-timestamp")
+    paginator = Paginator(all_posts, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    profile_follower = Follower.objects.filter(user=user_profile)
+    profile_follower_id = flatten(profile_follower.values_list('follower'))
+    if request.method == "POST":
+        if 'follow' in request.POST:
+            Follower.objects.create(user=user_profile, follower=request.user)
+        elif 'unfollow' in request.POST:
+            Follower.objects.get(user=user_profile, follower=request.user).delete()
+        return HttpResponseRedirect(reverse("profile", args=(user_id,)))
+    return render(request, "network/profile.html", {
+        "user_profile": user_profile,
+        "page_obj": page_obj,
+        "profile_follower_id": profile_follower_id,
+    })
